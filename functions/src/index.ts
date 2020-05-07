121d9df149ae951fb8f125dd072f59412f0c001e
Вано <vano.varderesyan94@gmail.com>
2020-05-07 10:52:07 +0400

    add paypay function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
+import paypal from 'paypal-rest-sdk';
 import * as soap from "soap";
 admin.initializeApp();
 
@@ -20,9 +21,105 @@ const TEMPLATE_ID = functions.config().sendgrid.template;
 
 sgMail.setApiKey(API_KEY);
 
+paypal.configure({
+	'mode': 'sandbox', //sandbox or live
+	'client_id': 'EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM',
+	'client_secret': 'EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM'
+});
+
+
+
+exports.pay = functions.https.onRequest((req, res) => {
+	// 1.Set up a payment information object, Build PayPal payment request
+	const payReq = JSON.stringify({
+		intent: 'sale',
+		payer: {
+			payment_method: 'paypal'
+		},
+		redirect_urls: {
+			return_url: `${req.protocol}://${req.get('host')}/process`,
+			cancel_url: `${req.protocol}://${req.get('host')}/cancel`
+		},
+		transactions: [{
+			amount: {
+				total: req.body.price,
+				currency: 'USD'
+			},
+			// This is the payment transaction description. Maximum length: 127
+			description: req.body.uid, // req.body.id
+			// reference_id string .Optional. The merchant-provided ID for the purchase unit. Maximum length: 256.
+			// reference_id: req.body.uid,
+			custom: req.body.uid,
+			// soft_descriptor: req.body.uid
+			// "invoice_number": req.body.uid,A
+		}]
+	});
+	// 2.Initialize the payment and redirect the user.
+	paypal.payment.create(payReq, (error:any, payment:any) => {
+		const links:any = {};
+		if (error) {
+			console.error(error);
+			res.status(500).end();
+		} else {
+			// Capture HATEOAS links
+			payment.links.forEach((linkObj:any) => {
+				links[linkObj.rel] = {
+					href: linkObj.href,
+					method: linkObj.method
+				};
+			});
+			// If redirect url present, redirect user
+			if (Object.prototype.hasOwnProperty.call(links, 'approval_url')) {
+				// REDIRECT USER TO links['approval_url'].href
+				console.info(links.approval_url.href);
+				// res.json({"approval_url":links.approval_url.href});
+				res.redirect(302, links.approval_url.href);
+			} else {
+				console.error('no redirect URI present');
+				res.status(500).end();
+			}
+		}
+	});
+});
+
+// 3.Complete the payment. Use the payer and payment IDs provided in the query string following the redirect.
+exports.process = functions.https.onRequest(async (req, res) => {
+	const paymentId = req.query.paymentId;
+	const payerId = {
+		payer_id: req.query.PayerID
+	};
+	const r = await paypal.payment.execute(paymentId, payerId, (error:any, payment:any) => {
+		if (error) {
+			console.error(error);
+			res.redirect(`${req.protocol}://${req.get('host')}/error`); // replace with your url page error
+		} else {
+			if (payment.state === 'approved') {
+				console.info('payment completed successfully, description: ', payment.transactions[0].description);
+				// console.info('req.custom: : ', payment.transactions[0].custom);
+				// set paid status to True in RealTime Database
+				const date = Date.now();
+				const uid = payment.transactions[0].description;
+				const ref = admin.database().ref('users/' + uid + '/');
+				ref.push({
+					'paid': true,
+					// 'description': description,
+					'date': date
+				})
+				res.redirect(`${req.protocol}://${req.get('host')}/success`); // replace with your url, page success
+			} else {
+				console.warn('payment.state: not approved ?');
+				// replace debug url
+				res.redirect(`https://console.firebase.google.com/project/${process.env.GCLOUD_PROJECT}/functions/logs?search=&severity=DEBUG`);
+			}
+		}
+	});
+	console.info('promise: ', r);
+});
+
+
 // Sends Payment
 export const payment = functions.https.onCall(async (data, context) => {
-	
+
 	const request = {
 		fullName: "Rahul Singh",
 		phone: "123456789",
@@ -37,37 +134,38 @@ export const payment = functions.https.onCall(async (data, context) => {
 	};
 	const result = await doPay(request);
 
-	return { success: true, result};
+	return { success: true, result };
 })
-const doPay = (request:any):Promise<any> => {
+
+const doPay = (request: any): Promise<any> => {
 	return new Promise((resolve, reject) => {
 		/*Local constible */
-		soap.createClient('https://privateqa.invoice4u.co.il/Services/ApiService.svc?singleWsdl',(err:any,soapLogin)=>{
-		
-		if(err){
+		soap.createClient('https://privateqa.invoice4u.co.il/Services/ApiService.svc?singleWsdl', (err: any, soapLogin) => {
+
+			if (err) {
 				console.log(err);
 			}
-			if(soapLogin){
+			if (soapLogin) {
 				var args = {
 					email: "test@test.com",
 					password: "123456"
-					};
-		soapLogin.VerifyLogin(args,(loginError:any,Loginresponse:any)=>{
-			if(loginError){
-				console.log(loginError);
-			}
-			console.log(Loginresponse);
-			
-			soapLogin.ProcessRequestFullContentsREST(request,(reqError:any, paymantResponse:any)=>{
-				if(reqError){
-					console.log(reqError);
-				}
-				resolve(paymantResponse.ProcessRequestFullContentsRESTResult);
-				
-			})
-			
-		})			
-				
+				};
+				soapLogin.VerifyLogin(args, (loginError: any, Loginresponse: any) => {
+					if (loginError) {
+						console.log(loginError);
+					}
+					console.log(Loginresponse);
+
+					soapLogin.ProcessRequestFullContentsREST(request, (reqError: any, paymantResponse: any) => {
+						if (reqError) {
+							console.log(reqError);
+						}
+						resolve(paymantResponse.ProcessRequestFullContentsRESTResult);
+
+					})
+
+				})
+
 			}
 		})
 	})
